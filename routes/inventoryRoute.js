1ff3c103f15eb429bd3e11b282096cf86a17fdf9
Tagg McDonald <taggmcd@gmail.com>
2024-03-23 10:18:20 -0600

    Add forms

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,8 +1,12 @@
+const utilities = require("../utilities/");
 const express = require("express");
 const router = new express.Router();
 const invController = require("../controllers/invController");
 
-router.get("/type/:classificationId", invController.buildByClassificationId);
-router.get("/detail/:invId", invController.detail);
+router.get(
+  "/type/:classificationId",
+  utilities.handleErrors(invController.buildByClassificationId)
+);
+router.get("/detail/:invId", utilities.handleErrors(invController.detail));
 
 module.exports = router;
